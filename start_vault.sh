2e6f78fd326747bf40c53d0269471abcf206df2e
Neil Edwards <neil@plasticwater.com>
2018-01-21 14:10:38 -0500

    more script changes

--- start_vault.sh
+++ start_vault.sh
@@ -1,9 +1,12 @@
 docker create -v /config --name config busybox; docker cp vault.hcl config:/config/;
 docker run -d --name consul      -p 8500:8500     consul:latest     agent -dev -client=0.0.0.0
 docker run -d --name vault   --link consul:consul   -p 8200:8200 --cap-add IPC_LOCK --volumes-from config  vault:latest server -config=/config/vault.hcl
+echo "Sleeping"
+sleep 10
+echo "Awake"
 alias vault='docker exec -it vault vault "$@"'
 docker exec -it vault vault init -address=${VAULT_ADDR} > keys.txt
-while [ ! -f /keys.txt ]
+while [ ! -f keys.txt ]
 do
   sleep 2
 done
@@ -11,4 +14,4 @@ docker exec -it vault vault unseal -address=${VAULT_ADDR} $(grep 'Key 1:' keys.t
 docker exec -it vault vault unseal -address=${VAULT_ADDR} $(grep 'Key 2:' keys.txt | awk '{print $NF}')
 docker exec -it vault vault unseal -address=${VAULT_ADDR} $(grep 'Key 3:' keys.txt | awk '{print $NF}')
 export VAULT_TOKEN=$(grep 'Initial Root Token:' keys.txt | awk '{print substr($NF, 1, length($NF)-1)}')
-docker exec -it vault "vault auth -address=${VAULT_ADDR} ${VAULT_TOKEN}"
+docker exec -it vault vault auth -address=${VAULT_ADDR} $(grep 'Initial Root Token:' keys.txt | awk '{print substr($NF, 1, length($NF)-1)}')
